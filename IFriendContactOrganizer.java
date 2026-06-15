35bc4c6822be9be8efcb741d2946e2bb7f057d0a
Chathurani Nimesha <chathunimesha389@gmail.com>
2026-06-15 19:05:22 +0530

    Updated file structure and Basic Home page

--- IFriendContactOrganizer.java
+++ IFriendContactOrganizer.java
@@ -1,5 +1,31 @@
 import java.util.*;
-
 class IFriendContactOrganizer {
-    
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+
+        System.out.printf("%79s", " /$$ /$$$$$$$$  /$$$$$$$  /$$$$$$ /$$$$$$$$ /$$   /$$ /$$$$$$$\n");
+        System.out.printf("%80s", "|__/| $$_______/| $$____$$|_  $$_/| $$_____/| $$$ | $$| $$__  $$\n");
+        System.out.printf("%80s", " /$$| $$        | $$    \\   | $$  | $$      | $$$$| $$| $$  \\ $$\n");
+        System.out.printf("%80s", "| $$| $$$$$     | $$$$$$$/  | $$  | $$$$$   | $$ $$ $$| $$  | $$\n");
+        System.out.printf("%80s", "| $$| $$__/     | $$__  $$  | $$  | $$__/   | $$  $$$$| $$  | $$\n");
+        System.out.printf("%80s", "| $$| $$        | $$  \\ $$  | $$  | $$      | $$\\  $$$| $$  | $$\n");
+        System.out.printf("%80s", "| $$| $$        | $$  | $$ /$$$$$$| $$$$$$$$| $$ \\  $$| $$$$$$$/\n");
+        System.out.printf("%79s", "|__/|__/        |__/  |__/|______/|________/|__/  \\__/|_______/\n");
+        System.out.println("\n\n  _____             _            _          ____                        _");
+        System.out.println(" / ____|           | |          | |        / __ \\                      (_)");
+        System.out.println("| |      ___  _ __ | |_ __ _  __| |_ __   | |  | |_ __ __ _  __ _ _ __  _ _______ _ __");
+        System.out.println("| |     / _ \\| '_ \\| __/ _' |/ _| __/ __| | |  | | '__/ _` |/ _` | '_ \\| |_  / _ \\ '__|");
+        System.out.println("| |____| (_) | | | | || |_| | |_| |_\\__ \\ | |__| | | | |_| | |_| | | | | |/ /  __/ |");
+        System.out.println(" \\______\\___/|_| |_|\\__\\__'_|\\__|\\__|___/  \\____/|_|  \\__, |\\__,_|_| |_|_/___\\___|_|");
+        System.out.println("                                                       __/ |");
+        System.out.println("                                                      |___/");
+        System.out.println("\n\n===================================================================================");
+        System.out.println("\n\t[01] ADD Contacts\n");
+        System.out.println("\t[02] UPDATE Contacts\n");
+        System.out.println("\t[03] DELETE Contacts\n");
+        System.out.println("\t[04] SEARCH Contacts\n");
+        System.out.println("\t[05] LIST Contacts\n");
+        System.out.println("\t[06] Exit\n");
+        System.out.print("Enter an option to continue -> ");
+    }
 }
